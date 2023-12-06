473dcba5545e065e8e6cc9a54bd30ab880f955a0
anukuljoshi <anukuljoshi1@gmail.com>
2023-12-06 21:13:59 +0530

    day 6, part 1 & part 2

---
+++ 2023/go/day06/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var test bool
+	flag.BoolVar(&test, "test", false, "use test file input")
+	flag.Parse()
+
+	filename := "./day06/input.txt"
+	if test {
+		filename = "./day06/test.txt"
+	}
+	input_file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	lines := make([]string, 0)
+	sc := bufio.NewScanner(input_file)
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+	answer1 := part1(lines)
+	fmt.Println("Part 1:", answer1)
+	answer2 := part2(lines)
+	fmt.Println("Part 2", answer2)
+}
+
+func convertToIntArray(s []string) []int {
+	res := make([]int, 0)
+	for _, c := range s {
+		n, err := strconv.Atoi(c)
+		if err != nil {
+			continue
+		}
+		res = append(res, n)
+	}
+	return res
+}
+
+func part1(lines []string) int {
+	res := 1
+	time := convertToIntArray(strings.Split(strings.Split(lines[0], ":")[1], " "))
+	distance := convertToIntArray(strings.Split(strings.Split(lines[1], ":")[1], " "))
+	for i := 0; i < len(time); i++ {
+		t := time[i]
+		d := distance[i]
+		l, r := 0, int(t/2)
+		for l < r {
+			m := l + int((r-l)/2)
+			if m*(t-m) > d {
+				r = m
+			} else {
+				l = m + 1
+			}
+		}
+		ways := (t + 1) - l*2
+		res *= ways
+	}
+	return res
+}
+
+func convertToString(s string) string {
+	res := ""
+	for _, c := range s {
+		_, err := strconv.Atoi(string(c))
+		if err != nil {
+			continue
+		}
+		res += string(c)
+	}
+	return res
+}
+
+func part2(lines []string) int {
+	t, _ := strconv.Atoi(convertToString(strings.Split(lines[0], ":")[1]))
+	d, _ := strconv.Atoi(convertToString(strings.Split(lines[1], ":")[1]))
+	fmt.Println(t, d)
+	l, r := 0, int(t/2)
+	for l < r {
+		m := l + int((r-l)/2)
+		if m*(t-m) > d {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+	ways := (t + 1) - l*2
+	return ways
+}
